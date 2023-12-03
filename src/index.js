9974312ccb730f18d0372394e5bbd2684aa5c154
mario <mariorb2017@gmail.com>
2023-12-03 20:37:42 +0100

    Add edit todo functionality

--- src/index.js
+++ src/index.js
@@ -16,7 +16,6 @@ const toDoList = document.querySelector(".todo-list");
 const projectSelect = document.querySelector("#project-select");
 
 
-
 function displayToDos(){
     toDos.myToDos.map(todo => {
         let toDoId = toDos.myToDos.indexOf(todo)
@@ -27,25 +26,149 @@ function displayToDos(){
             toDoTitle.classList.add("toDoTitle");
             toDoTitle.textContent = `${todo.title}`;
             let dueDate = document.createElement("p");
+            dueDate.classList.add("toDoDueDate")
             dueDate.textContent = `${dateFns.format(new Date(todo.dueDate), 'dd-MM-yy')}`;
             let priority = document.createElement("div");
             priority.classList.add("priority");
             priority.setAttribute("id", `${todo.priority}-priority`);
             let project = document.createElement("p");
+            project.classList.add("toDoProject")
             project.textContent = todo.project;
+            let todoBtns = document.createElement("div");
+            todoBtns.classList.add("todo-buttons-div")
+            let editBtn = document.createElement("div");
+            editBtn.classList.add("edit-todo");
+            editBtn.classList.add("material-icons");
+            editBtn.textContent = "edit";
             let deleteBtn = document.createElement("div");
             deleteBtn.classList.add("delete-todo");
             deleteBtn.classList.add("material-icons");
             deleteBtn.textContent = "delete"
-        
+            
             todoLi.appendChild(toDoTitle);
             todoLi.appendChild(dueDate);
             todoLi.appendChild(priority);
             todoLi.appendChild(project);
-            todoLi.appendChild(deleteBtn);
-
+            todoBtns.appendChild(editBtn);
+            todoBtns.appendChild(deleteBtn);
+            todoLi.appendChild(todoBtns);
             toDoList.appendChild(todoLi);
         }
+    })
+};
+
+function changeToDoTitleToInput(parentToDo, editDiv){
+    let toDoTitle = parentToDo.querySelector(".toDoTitle");
+    toDoTitle.hidden = true;
+    let toDoTitleInput = document.createElement("input");
+    toDoTitleInput.setAttribute("value", toDoTitle.textContent);
+    toDoTitleInput.setAttribute("id", "editToDoTitle")
+    editDiv.appendChild(toDoTitleInput);
+}
+function changeToDoDueDateToInput(parentToDo, editDiv){
+    let toDoDueDate = parentToDo.querySelector(".toDoDueDate")
+    toDoDueDate.hidden = true;
+    let toDoDueDateInput = document.createElement("input");
+    toDoDueDateInput.setAttribute("type", "date")
+    toDoDueDateInput.setAttribute("value", toDoDueDate.textContent);
+    toDoDueDateInput.setAttribute("id", "editToDoDueDate")
+    editDiv.appendChild(toDoDueDateInput);
+}
+function changeToDoPriorityToInput(parentToDo){
+    let toDoPriority = parentToDo.querySelector(".priority");
+    toDoPriority.style.cursor = "pointer";
+    parentToDo.addEventListener("click", (e) => {
+        let toDoPriorityChange = e.target.closest(".priority");
+        if(toDoPriorityChange){
+            let selectedPriority = toDoPriority.id.split("-")[0]
+            if(selectedPriority === "high"){
+                toDoPriority.id = "medium-priority"
+            } else if (selectedPriority === "medium"){
+                toDoPriority.id = "low-priority"
+            } else {
+                toDoPriority.id = "high-priority"
+            }
+        }
+    });
+}
+function changeToDoButtons(parentToDo, e){
+    let buttonsDiv = parentToDo.querySelector(".todo-buttons-div");
+    console.log(e.target)
+    let editButton = e.target;
+    editButton.style.display = "none";
+    let confirmButton = document.createElement("div");
+    confirmButton.classList.add("confirm-change-todo");
+    confirmButton.classList.add("material-icons");
+
+    let deleteButton = e.target.nextSibling;
+    deleteButton.style.display = "none";
+
+    let cancelButton = document.createElement("div");
+    cancelButton.classList.add("cancel-change-todo");
+    cancelButton.classList.add("material-icons");
+    
+    confirmButton.textContent = "check";
+    cancelButton.textContent = "close";
+
+    buttonsDiv.appendChild(confirmButton);
+    buttonsDiv.appendChild(cancelButton);
+}
+
+function changeToDoInfoToInputs(parentToDo, e){
+    let editDiv = document.createElement("div");
+    editDiv.classList.add("edit-div");
+    changeToDoTitleToInput(parentToDo, editDiv);
+    changeToDoDueDateToInput(parentToDo, editDiv);
+    parentToDo.insertBefore(editDiv, parentToDo.querySelector(".priority"))
+    changeToDoPriorityToInput(parentToDo);
+    changeToDoButtons(parentToDo, e);
+}
+
+function finishChangesButtons(parentToDo){
+    const confirmBtn = parentToDo.querySelector(".confirm-change-todo");
+    const editBtn = parentToDo.querySelector(".edit-todo");
+    const cancelBtn = parentToDo.querySelector(".cancel-change-todo")
+    const deleteBtn = parentToDo.querySelector(".delete-todo");
+    const editDiv = parentToDo.querySelector(".edit-div");
+
+    confirmBtn.remove();
+    cancelBtn.remove();
+    
+    editBtn.removeAttribute("style");
+    deleteBtn.removeAttribute("style")
+    editDiv.remove();
+}
+
+function editToDo(e){
+    const parentToDo = e.target.parentElement.parentElement
+    changeToDoInfoToInputs(parentToDo, e);
+    parentToDo.addEventListener("click", (event) => {
+    const confirmButton = event.target.closest(".confirm-change-todo")
+    const cancelButton = event.target.closest(".cancel-change-todo");
+
+    
+
+    const toDoTitle = parentToDo.querySelector(".toDoTitle");
+    const toDoDueDate = parentToDo.querySelector(".toDoDueDate");
+    
+    
+    if(confirmButton){
+        const toDoIndex = parentToDo.id.split("")[2];
+        toDos.myToDos[toDoIndex].title = parentToDo.querySelector("#editToDoTitle").value;
+        toDoTitle.textContent = toDos.myToDos[toDoIndex].title;
+        toDoTitle.hidden = false;
+        toDos.myToDos[toDoIndex].dueDate = parentToDo.querySelector("#editToDoDueDate").value;
+        toDoDueDate.textContent = toDos.myToDos[toDoIndex].dueDate;
+        toDoDueDate.hidden = false;
+        toDos.myToDos[toDoIndex].priority = parentToDo.querySelector(".priority").id.split("-")[0];
+        finishChangesButtons(parentToDo);
+        
+    }
+    if(cancelButton){
+        toDoTitle.hidden = false;
+        toDoDueDate.hidden = false;
+        finishChangesButtons(parentToDo);
+    }
     })
 }
 
@@ -193,7 +316,8 @@ document.addEventListener("DOMContentLoaded", (e) => {
 
 document.addEventListener("click", (e) => {
     const deleteBtn = e.target.closest(".delete-todo");
-    const confirmNewProjectBtn = e.target.closest(".confirm-new-project")
+    const confirmNewProjectBtn = e.target.closest(".confirm-new-project");
+    const editBtn = e.target.closest(".edit-todo");
     if(deleteBtn){
         deleteToDo(e);
     }
@@ -201,6 +325,10 @@ document.addEventListener("click", (e) => {
         createNewProject();
         displayProjects();
         toggleNewProject();
+    }
+    if(editBtn){
+        editToDo(e);
+        
     }
 })
 
--- src/todos.js
+++ src/todos.js
@@ -1,41 +1,41 @@
 export const myToDos = [{
     title: "Go to the gym",
-    dueDate: "2023/12/2",
+    dueDate: "2023-12-2",
     priority: "high",
     project: "default",
     id : 0
 },
 {
     title: "Go to the gym",
-    dueDate: "2023/12/2",
+    dueDate: "2023-12-2",
     priority: "high",
     project: "default",
     id : 1
 },
 {
     title: "Go to the gym",
-    dueDate: "2023/12/6",
-    priority: "high",
+    dueDate: "2023-12-6",
+    priority: "low",
     project: "default",
     id : 2
 },
 {
     title: "Go to the gym",
-    dueDate: "2023/12/5",
+    dueDate: "2023-12-5",
     priority: "high",
     project: "default",
     id : 3
 },
 {
     title: "Go to the gym",
-    dueDate: "2023/12/1",
-    priority: "high",
+    dueDate: "2023-12-1",
+    priority: "medium",
     project: "default",
     id : 4
 },
 {
     title: "Go to the gym",
-    dueDate: "2023/12/3",
+    dueDate: "2023-12-3",
     priority: "high",
     project: "default",
     id : 5
